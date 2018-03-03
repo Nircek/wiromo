0f7a8c30ce1330cf1910769b31272a1fbf80ab6d
Marcin Zepp <m.zepp@outlook.com>
2018-03-03 19:00:41 +0100

    Add base structure

--- wiromo.ino
+++ wiromo.ino
@@ -1,7 +1,23 @@
-void setup(){
+//includes
 
+//output pins defines
+
+//input pins defines
+
+//mode define
+
+//IR commands defines
+
+//function definitions with their globals
+
+
+void setup(){
+  //setup
 }
 
 void loop(){
-
+  //loop
 }
+
+//function bodys
+
