0c236c91f5b49791c38d865f85ebd8cc0e005824
Marcin Zepp <m.zepp@outlook.com>
2018-03-04 08:23:58 +0100

    Add TODOs

--- wiromo.ino
+++ wiromo.ino
@@ -1,15 +1,14 @@
 #include <IRremote.h>
 //get it from https://github.com/z3t0/Arduino-IRremote
 
-//output pins defines
+//TODO: output pins defines
 
-//input pins defines
+//TODO: input pins defines
 
-//mode define
+//TODO: mode define
 
-//IR commands defines
+//TODO: IR commands defines
 
-//function definitions with their globals
 void setPins();
 
 //global mode
@@ -17,21 +16,28 @@ int gMode=0; //zmienna przechowująca aktualnie włączony tryb
 //change mode
 void chMode(); //funkcja wywoływana po naciśnięciu przycisku zmiany trybu
 void updateModeLED(); //odświerza stan diody trybów
+//TODO: functions of loop for each mode
 
+//TODO: functions for motor management
 
 void setup(){
   //setup
   setPins();
+  //TODO: begin BT Serial
 }
 
 void loop(){
   //loop
   updateModeLED();
+  //TODO: switch gMode with cases of modes
 }
 
 //function bodys
 void setPins(){
-  //interrupt connected between button pin and chMode()
+  //TODO: set LOW voltage on motors
+  //TODO: set pin mode of each pin
+  //TODO: enable IR
+  //TODO: interrupt connected between button pin and chMode()
 }
 
 void chMode(){
